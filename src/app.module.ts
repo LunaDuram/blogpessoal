71fb8dba8e98962f00aee74a0c92280d1bbd6a2f
LunaDuram <lucineiaduram@gmail.com>
2022-10-13 08:05:39 -0300

    Concluído

---
+++ src/app.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Postagem } from './postagem/entities/postagem.entity';
+import { PostagemModule } from './postagem/postagem.module';
+import { Tema } from './tema/entities/tema.entity';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+     type: 'mysql',
+    host: 'localhost',
+    port: 3306,
+    username: 'root',
+    password: 'root',
+    database: 'db_blogpessoal',
+    entities: [Postagem, Tema],
+    synchronize: true
+    }),
+    PostagemModule
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
